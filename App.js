77a632226d4f861e9b1e9cf1195e1e4d3046b7b2
Afshin Mokhtari <afshinator@hotmail.com>
2020-09-27 15:53:05 -0700

    convert to class component for now

--- App.js
+++ App.js
@@ -1,100 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import daysjs from "dayjs";
 
-const { width } = Dimensions.get('screen')
-const SIZE = width *  0.9
+const { width } = Dimensions.get("screen");
+const SIZE = width * 0.9;
 
-export default function App() {
-  const rotateSeconds = '25deg'
-  const transformSeconds = {
-    transform: [{ rotate: rotateSeconds}]
-  }
-  const rotateMinutes = '125deg'
-  const transformMinutes = {
-    transform: [{ rotate: rotateMinutes}]
-  }
-  const rotateHours = '225deg'
-  const transformHours = {
-    transform: [{ rotate: rotateHours }]
-  }
+export default class App extends React.Component {
+  render() {
+    const rotateSeconds = "25deg";
+    const transformSeconds = {
+      transform: [{ rotate: rotateSeconds }],
+    };
+    const rotateMinutes = "125deg";
+    const transformMinutes = {
+      transform: [{ rotate: rotateMinutes }],
+    };
+    const rotateHours = "225deg";
+    const transformHours = {
+      transform: [{ rotate: rotateHours }],
+    };
 
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden={true} />
-      <View style={[styles.big]} />
-      <View style={[styles.medium]} />
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        <View style={[styles.big]} />
+        <View style={[styles.medium]} />
 
-      <View style={[styles.mover, transformHours]} >
-        <View style={[styles.hours]} />
+        <View style={[styles.mover, transformHours]}>
+          <View style={[styles.hours]} />
+        </View>
+        <View style={[styles.mover, transformMinutes]}>
+          <View style={[styles.minutes]} />
+        </View>
+        <View style={[styles.mover, transformSeconds]}>
+          <View style={[styles.seconds]} />
+        </View>
+        <View style={[styles.small]} />
       </View>
-      <View style={[styles.mover, transformMinutes]} >
-        <View style={[styles.minutes]} />
-      </View>
-      <View style={[styles.mover, transformSeconds]} >
-        <View style={[styles.seconds]} />
-      </View>
-      <View style={[styles.small]} />
-
-
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   mover: {
-    position: 'absolute',
+    position: "absolute",
     width: SIZE,
     height: SIZE,
-    borderRadius: SIZE/2,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    borderRadius: SIZE / 2,
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   hours: {
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
-    height: '35%',
-    marginTop: '15%',
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    height: "35%",
+    marginTop: "15%",
     width: 4,
-    borderRadius: 4
+    borderRadius: 4,
   },
   minutes: {
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    height: '45%',
-    marginTop: '5%',
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    height: "45%",
+    marginTop: "5%",
     width: 3,
     borderRadius: 3,
   },
   seconds: {
-    backgroundColor: 'rgba(234, 128, 252, 1)',
-    height: '50%',
+    backgroundColor: "rgba(234, 128, 252, 1)",
+    height: "50%",
     width: 2,
-    borderRadius: 2
+    borderRadius: 2,
   },
   big: {
-    position: 'absolute',
+    position: "absolute",
     width: SIZE * 0.8,
     height: SIZE * 0.8,
     borderRadius: SIZE * 0.4,
-    backgroundColor: 'rgba(200, 200, 200, 0.2)',
+    backgroundColor: "rgba(200, 200, 200, 0.2)",
   },
   medium: {
-    position: 'absolute',
+    position: "absolute",
     width: SIZE * 0.5,
     height: SIZE * 0.5,
     borderRadius: SIZE * 0.25,
-    backgroundColor: 'rgba(200, 200, 200, 0.4)',
+    backgroundColor: "rgba(200, 200, 200, 0.4)",
   },
   small: {
-    position: 'absolute',
+    position: "absolute",
     width: 10,
     height: 10,
     borderRadius: 5,
-    backgroundColor: 'rgba(234, 128, 252, 1)',
+    backgroundColor: "rgba(234, 128, 252, 1)",
   },
   container: {
     flex: 1,
-    backgroundColor: '#292929',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
